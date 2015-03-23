7a6c05a532bae4a022af683a48f47d4e1215f89c
ikekonglp <ikekonglp@gmail.com>
2015-03-23 11:08:47 -0400

    log to stderr, ignore unsigned size compare

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "dependencyutils/TokenDictionary.h"
 #include "dependencyutils/DependencyDictionary.h"
 #include "dependencyutils/DependencyReader.h"
+#include "dependencyutils/DependencyWriter.h"
 #include <glog/logging.h>
 #include <gflags/gflags.h>
 
@@ -30,6 +31,19 @@ void dependency_prepare(){
     // Do whatever you like here, token dictionary and dependeny dictionary have been built
     cout << token_dictionary->GetNumForms() << endl;
 
+    string outputf = "/dev/stdout";
+
+    DependencyWriter* writer = new DependencyWriter;
+    writer->Open(outputf);
+
+    reader->Open(file_name);
+    
+    DependencyInstance* instance = static_cast<DependencyInstance*> (reader->GetNext());
+    writer->Write(instance);
+
+
+    reader->Close();
+    writer->Close();
 }
 
 int main(int argc, char** argv)
@@ -38,7 +52,7 @@ int main(int argc, char** argv)
     // if google libs are installed in local lib path
     google::InitGoogleLogging(argv[0]);
     google::ParseCommandLineFlags(&argc, &argv, true);
-
+    google::LogToStderr();
     // LispTree lt;
     // string s = "(S (NP (NP (NNP Burmah)) (, ,) (SBAR (WHNP (WDT which)) (S (VP (VBZ owns) (NP (NP (DT the) (NNP Castrol) (NN brand)) (PP (IN of) (NP (JJ lubricant) (NNS oils))))))) (, ,)) (VP (VBD reported) (NP (NP (DT a) (ADJP (CD 17) (NN %)) (NN rise)) (PP (IN in) (NP (JJ net) (NN income))) (PP (TO to) (NP (NP (QP (# #) (CD 43.5) (CD million))) (PRN (-LRB- -LRB-) (NP (QP ($ $) (CD 68.3) (CD million))) (-RRB- -RRB-)))) (PP (IN in) (NP (DT the) (JJ first) (NN half))))) (. .))";
     // lt.read_from_string(s);
@@ -81,7 +95,7 @@ void read_by_line(){
 
     read_lines_into_vector(file_name, back_inserter(lines));
 
-    for (int i = 0; i < lines.size(); i++){
+    for (unsigned int i = 0; i < lines.size(); i++){
         // cout << lines[i] << endl;
         boost::trim(lines[i]);
         if(lines[i].compare("") == 0){
@@ -90,7 +104,7 @@ void read_by_line(){
         }
         vector<string> strs;
         boost::split(strs, lines[i], boost::is_any_of("\t"));
-        for (int j = 0; j < strs.size(); j++){
+        for (unsigned int j = 0; j < strs.size(); j++){
             cout << strs[j] << " ";
         }
         cout << endl;
