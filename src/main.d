79b1e14b3aa34b0b984a08adc22d34ff0f7f8143
ikekonglp <ikekonglp@gmail.com>
2015-03-24 15:54:15 -0400

    archive

---
+++ src/main.d
@@ -0,0 +1,11 @@
+main.o main.d: main.cpp io_utils.hpp treeutils/lisp.h \
+ dependencyutils/TokenDictionary.h dependencyutils/Dictionary.h \
+ dependencyutils/Alphabet.h dependencyutils/Utils.h \
+ dependencyutils/TimeUtils.h dependencyutils/StringUtils.h \
+ dependencyutils/DependencyReader.h dependencyutils/DependencyInstance.h \
+ dependencyutils/Instance.h dependencyutils/Reader.h \
+ dependencyutils/DependencyDictionary.h dependencyutils/TokenDictionary.h \
+ dependencyutils/SerializationUtils.h dependencyutils/DependencyReader.h \
+ dependencyutils/DependencyWriter.h dependencyutils/Writer.h \
+ dependencyutils/DependencyInstanceNumeric.h \
+ dependencyutils/DependencyDictionary.h
